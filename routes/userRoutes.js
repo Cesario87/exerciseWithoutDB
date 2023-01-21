04ac9fb3bcb76d73a232415a7cd0b8899c7643be
Cesario87 <ces.albred@gmail.com>
2023-01-21 10:04:46 +0100

    test main

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,8 +4,8 @@ const usersRouter = express.Router();
 const userController = require ('../controllers/userController');
 
 usersRouter.get('/users', userController.getUsers);
-usersRouter.get('/users/:username?', userController.getOneUser);
 usersRouter.get('/total', userController.getTotalUsers);
+usersRouter.get('/users/:username?', userController.getOneUser);
 usersRouter.get('/users/country/:country?', userController.getCountryUsers);
 
 
