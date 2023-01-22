d6a55a9aea8f5acff765e3f0170452c292302856
Cesario87 <ces.albred@gmail.com>
2023-01-22 18:17:40 +0100

    completed, but to be reviewed

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const getUsers = (req, res) => {
     const usersJson = fs.readFileSync("db/users.json", "utf8");
     const users = JSON.parse(usersJson);
+    console.log(users);
     res.status(200).json(users);
 }
 //2
@@ -37,33 +38,113 @@ const getCountryUsers = (req, res) => {
 }
 //5
 const getVehicleUsers = (req, res) => {
-  // let user = "holi";
-  // res.status(200).json(user);
     const usersJson = fs.readFileSync("db/users.json", "utf8");
     const users = JSON.parse(usersJson);
     const min = req.query.min;
     const max = req.query.max;
-  
+
     // Aquí se asume que la variable "users" contiene la información de los usuarios
     const filteredUsers = users.filter(user => {
-      return user.vehicles.length >= min && user.vehicles.length <= max;
+        return user.vehicles.length >= min && user.vehicles.length <= max;
     });
-  
+
     const response = filteredUsers.map(user => {
-      return {
-        email: user.email,
-        username: user.username,
-        img: user.img
-      }
+        return {
+            email: user.email,
+            username: user.username,
+            img: user.img
+        }
     });
-  
+
     res.send(response);
-  };
+};
+//6
+const getOneFoodUsers = (req, res) => {
+    const usersJson = fs.readFileSync("db/users.json", "utf8");
+    const users = JSON.parse(usersJson);
+    const food = req.params.food;
+    const usersWithFood = users.filter(user => user.favouritesFood.includes(food));
+    res.json(usersWithFood);
+};
+//7
+const getFoodUsers = (req, res) => {
+    const usersJson = fs.readFileSync("db/users.json", "utf8");
+    const users = JSON.parse(usersJson);
+    let foods = [];
+    for (let i = 0; i < users.length; i++) {
+        foods = foods.concat(users[i].favouritesFood);
+    }
+    // Eliminar comidas duplicadas
+    foods = [...new Set(foods)];
+    res.status(200).json(foods);
+}
+
+//8
+const getVehiclesUsers = (req, res) => {
+    const usersJson = fs.readFileSync("db/users.json", "utf8");
+    const users = JSON.parse(usersJson);
+    let fuel = req.query.fuel;
+    let manufacturer = req.query.manufacturer;
+    let model = req.query.model;
+
+    let filteredUsers = users.filter(user => {
+        let hasVehicle = false;
+        if (fuel || manufacturer || model) {
+            user.vehicles.forEach(vehicle => {
+                if ((!fuel || vehicle.fuel === fuel) &&
+                    (!manufacturer || vehicle.manufacturer === manufacturer) &&
+                    (!model || vehicle.model === model)) {
+                    hasVehicle = true;
+                }
+            });
+        } else {
+            hasVehicle = true;
+        }
+        return hasVehicle;
+    });
+
+    const response = filteredUsers.map(user => {
+        return {
+            email: user.email,
+            username: user.username,
+            img: user.img
+        }
+    });
+
+    res.json(response);
+};
+
+//9
+const getVehicles2Users = (req, res) => {
+    const usersJson = fs.readFileSync("db/users.json", "utf8");
+    const users = JSON.parse(usersJson);
+    let fuel = req.query.fuel;
+    let filteredVehicles = [];
+
+    users.forEach(user => {
+      filteredVehicles = filteredVehicles.concat(user.vehicles.filter(vehicle => {
+        return !fuel || vehicle.fuel === fuel;
+      }));
+    });
+
+    let uniqueVehicles = [...new Set(filteredVehicles.map(item => JSON.stringify(item)))].map(str => JSON.parse(str));
+    let vehiclesCount = {};
+    uniqueVehicles.forEach(vehicle => {
+        let count = filteredVehicles.filter(v => v.manufacturer === vehicle.manufacturer && v.model === vehicle.model).length;
+        vehiclesCount[vehicle.manufacturer + ' ' + vehicle.model] = count;
+    });
+    res.json({ vehicles: uniqueVehicles, count: vehiclesCount });
+};
 
 module.exports = {
     getOneUser,
     getUsers,
     getTotalUsers,
     getCountryUsers,
-    getVehicleUsers
+    getVehicleUsers,
+    getOneFoodUsers,
+    getFoodUsers,
+    getVehiclesUsers,
+    getVehicles2Users,
+
 }
---
+++ controllers/userPutController.js
@@ -0,0 +1,122 @@
+const fs = require('fs');
+//11
+const putUsers = (req, res) => {
+    const username = req.params.username;
+    const firstname = req.body.firstName;
+    const lastname = req.body.lastName;
+    const email = req.body.email;
+    
+    if (username) {
+        const usersJson = fs.readFileSync("db/users.json", "utf8");
+        const users = JSON.parse(usersJson);
+        let userIndex = users.findIndex(user => user.username === username);
+        if (userIndex !== -1) {
+            users[userIndex].firstname = firstname || users[userIndex].firstname;
+            users[userIndex].lastname = lastname || users[userIndex].lastname;
+            users[userIndex].email = email || users[userIndex].email;
+            fs.writeFileSync('db/users.json', JSON.stringify(users), 'utf8');
+            res.status(200).json({firstname: users[userIndex].firstname, lastname: users[userIndex].lastname, email: users[userIndex].email});
+        } else {
+            res.status(404).json({
+                error: 'User not found'
+            });
+        }
+    } else {
+        res.status(400).json({
+            error: 'Username is required to update a user'
+        });
+    }
+};
+//12
+const putVehicleUsers = (req, res) => {
+    const username = req.params.username;
+    const firstname = req.body.firstName;
+    const lastname = req.body.lastName;
+    const email = req.body.email;
+    const vehicles = req.body.vehicles;
+
+    
+    if (username) {
+        const usersJson = fs.readFileSync("db/users.json", "utf8");
+        const users = JSON.parse(usersJson);
+        let userIndex = users.findIndex(user => user.username === username);
+        if (userIndex !== -1) {
+            users[userIndex].vehicles = vehicles || users[userIndex].vehicles;
+            users[userIndex].firstname = firstname || users[userIndex].firstname;
+            users[userIndex].lastname = lastname || users[userIndex].lastname;
+            users[userIndex].email = email || users[userIndex].email;
+            fs.writeFileSync('db/users.json', JSON.stringify(users), 'utf8');
+            res.status(200).json({firstname: users[userIndex].firstname, lastname: users[userIndex].lastname, email: users[userIndex].email, vehicles: users[userIndex].vehicles});
+        } else {
+            res.status(404).json({
+                error: 'User not found'
+            });
+        }
+    } else {
+        res.status(400).json({
+            error: 'Username is required to update a user'
+        });
+    }
+};
+//13
+const putFoodUsers = (req, res) => {
+    const username = req.params.username;
+    const firstname = req.body.firstName;
+    const lastname = req.body.lastName;
+    const email = req.body.email;
+    const favouritesFood = req.body.food;
+
+    
+    if (username) {
+        const usersJson = fs.readFileSync("db/users.json", "utf8");
+        const users = JSON.parse(usersJson);
+        let userIndex = users.findIndex(user => user.username === username);
+        if (userIndex !== -1) {
+            users[userIndex].favouritesFood = favouritesFood || users[userIndex].favouritesFood;
+            users[userIndex].firstname = firstname || users[userIndex].firstname;
+            users[userIndex].lastname = lastname || users[userIndex].lastname;
+            users[userIndex].email = email || users[userIndex].email;
+            fs.writeFileSync('db/users.json', JSON.stringify(users), 'utf8');
+            res.status(200).json({firstname: users[userIndex].firstname, lastname: users[userIndex].lastname, email: users[userIndex].email, favouritesFood: users[userIndex].favouritesFood});
+        } else {
+            res.status(404).json({
+                error: 'User not found'
+            });
+        }
+    } else {
+        res.status(400).json({
+            error: 'Username is required to update a user'
+        });
+    }
+};
+//14
+const putHideUsers = (req, res) => {
+    const username = req.params.username;
+    const email = req.body.email;
+  
+    if (username && email) {
+      const usersJson = fs.readFileSync("db/users.json", "utf8");
+      const users = JSON.parse(usersJson);
+      let userIndex = users.findIndex(user => user.username === username && user.email === email);
+      if (userIndex !== -1) {
+        users[userIndex].visibility = false;
+        fs.writeFileSync('db/users.json', JSON.stringify(users), 'utf8');
+        res.status(200).json({message: 'User visibility has been successfully updated'});
+      } else {
+        res.status(404).json({
+          error: 'User not found'
+        });
+      }
+    } else {
+      res.status(400).json({
+        error: 'Both username and email are required to hide a user'
+      });
+    }
+  };
+
+module.exports = {
+    putUsers,
+    putVehicleUsers,
+    putFoodUsers,
+    putHideUsers
+}
